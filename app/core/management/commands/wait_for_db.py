4dc62e9cecae6ff5db9b5af49aca8d10709621a2
morteza <sr.morteza@gmail.com>
2022-11-02 09:25:53 +0330

    add deb check ready

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to wait for database."""
+
+    def handle(self, *args, **options):
+        pass
